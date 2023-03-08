f1358d06aa3ef3db486bd2c0a9c011d522c95633
sreeventh <sreeventhsrinivasan@gmail.com>
2023-03-08 23:28:31 +0530

    dropdown

---
+++ lib/dropdown.dart
@@ -0,0 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class DropdownAddr extends StatefulWidget {
+  const DropdownAddr({Key? key}) : super(key: key);
+
+  @override
+  State<DropdownAddr> createState() => _DropdownAddrState();
+}
+
+class _DropdownAddrState extends State<DropdownAddr> {
+  String? _selectedValue;
+  List<String> _dropdownValues = [];
+  String? uid;
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton(
+      value: _selectedValue,
+      items: _dropdownValues.map((value) {
+        return DropdownMenuItem(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+      onChanged: (value) {
+        setState(() {
+          _selectedValue = value;
+        });
+      },
+    );
+  }
+
+  void _loadDropdownValues() async {
+    final usersCollection = FirebaseFirestore.instance.collection('users');
+    final userDoc = await usersCollection.doc(uid).get();
+    final addrList = userDoc.get('addr') as List<dynamic>;
+    final values = addrList.map((addr) => addr.toString()).toList();
+    setState(() {
+      _dropdownValues = values;
+    });
+  }
+
+  Future<void> getData() async {
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    uid = pref.getString('uid');
+    _loadDropdownValues();
+    setState(() {});
+  }
+}
--- lib/road.dart
+++ lib/road.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:delta/dropdown.dart';
 
 class Road extends StatefulWidget {
   const Road({super.key});
@@ -70,6 +71,7 @@ class _RoadState extends State<Road> {
           ),
         ),
       ),
+      bottomNavigationBar: const DropdownAddr(),
     );
   }
 
